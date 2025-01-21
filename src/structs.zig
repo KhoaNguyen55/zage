f9ea73bc2339c107524e1ad47cb9a5227ed9b9de
KhoaNguyen55 <khoa@khoa-nguyen.net>
2025-01-21 13:38:15 -0500

    finish x25519identity parser

--- src/structs.zig
+++ src/structs.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const X25519 = std.crypto.dh.X25519;
+const bech32 = @import("bech32.zig");
 const Allocator = std.mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
 const ArrayList = std.ArrayList;
@@ -226,17 +228,37 @@ const AnyIdentity = struct {
     }
 };
 
-const X25519Identity = struct {
-    secret_key: []u8,
-    our_public_key: []u8,
+const secret_key_hrp = "AGE-SECRET-KEY-";
+const public_key_hrp = "age";
 
-    pub fn parse(secret_key: []const u8) !X25519Identity {
-        _ = secret_key;
-        // TODO: check if its valid key
-        // TODO: get public from secret key
+const X25519Identity = struct {
+    secret_key: [X25519.secret_length]u8,
+    our_public_key: [X25519.public_length]u8,
+
+    pub fn parse(key: []const u8) !X25519Identity {
+        var buffer: [512]u8 = undefined;
+        var fba = std.heap.FixedBufferAllocator.init(&buffer);
+        const allocator = fba.allocator();
+
+        const decoded_key = try bech32.decode(allocator, key);
+        defer {
+            allocator.free(decoded_key.hrp);
+            allocator.free(decoded_key.data);
+        }
+
+        if (!std.mem.eql(u8, decoded_key.hrp, secret_key_hrp)) {
+            return Error.WrongKeyHrp;
+        }
+
+        if (decoded_key.data.len != X25519.secret_length) {
+            return Error.InvalidX25519SecretKey;
+        }
+        const secret_key = decoded_key.data[0..X25519.secret_length].*;
+        const public_key = try X25519.recoverPublicKey(secret_key);
+
         return X25519Identity{
-            .secret_key = undefined,
-            .our_public_key = undefined,
+            .secret_key = secret_key,
+            .our_public_key = public_key,
         };
     }
 
@@ -258,7 +280,14 @@ fn decrypt(identity: AnyIdentity) !void {
 }
 
 test "Identity test" {
-    const test_string: []const u8 = "wip";
-    const x25519 = try X25519Identity.parse(test_string);
+    const expected_sec_key = "AGE-SECRET-KEY-1QGN768HAM3H3SDL9WRZZYNP9JESEMEQFLFSJYLZE5A52U55WM2GQH8PMPW";
+    const expected_pub_key = "age17mt2y8v5f3chc5dv22jz4unfcqey37v9jtxlcq834hx5cytjvp6s9txfk0";
+    const x25519 = try X25519Identity.parse(expected_sec_key);
+    const sec_key = try bech32.encode(test_allocator, secret_key_hrp, &x25519.secret_key);
+    defer test_allocator.free(sec_key);
+    const pub_key = try bech32.encode(test_allocator, public_key_hrp, &x25519.our_public_key);
+    defer test_allocator.free(pub_key);
+    try testing.expectEqualStrings(pub_key, expected_pub_key);
+    try testing.expectEqualStrings(sec_key, expected_sec_key);
     try decrypt(x25519.any());
 }
